25e433a5643d8eb6e820511fded3c63e5aed7ea1
TXActionAlerts <dbcharles@me.com>
2025-08-07 22:15:16 -0500

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,13 @@
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-      "./public/*.html"
-    ],
-    theme: {
-      extend: {
-        colors: {
-          'wsr-navy': '#202b41',
-          'wsr-red': '#b71c1c',
-          'wsr-gray': '#eef0f3'
-        }
-      },
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'wsr-navy': '#202b41',
+        'wsr-red': '#b71c1c',
+        'wsr-gray': '#eef0f3'
+      }
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+}
